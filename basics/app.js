4ccc259c94fc2c86274bb449fbee9aac0cb32361
Glushko Oleksandr <stalevar.1990@gmail.com>
2025-04-17 22:31:02 +0300

    using module system

--- basics/app.js
+++ basics/app.js
@@ -1,48 +1,9 @@
 
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
 
-const server = http.createServer((request, response) => {
-    const url = request.url;
-    const method = request.method;
-
-
-    if (url === '/') {
-        response.setHeader('Content-Type', 'text/html');
-        response.write('<html>');
-        response.write('<head><title>My First Page</title></head>');
-        response.write('<body><form action="/message" method="POST"><input name="message" type="text"></input><button type="submit">Send</button></form></body>');
-        response.write('</html>');
-        return response.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        return request.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                if (err) {
-                    console.error(err);
-                    return response.end('Error writing file');
-                }
-                response.statusCode = 302;
-                response.setHeader('Location', '/');
-                return response.end();
-            });
-            
-        });
-    }
-
-
-    response.statusCode =404
-    return response.end('<h1>Page not foutd</h1>');
-
-});
+const server = http.createServer(routes.handler);
 
 
 server.listen(3000, () => {
