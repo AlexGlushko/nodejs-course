f7da3662ffe1f00b7f36396d8a61f5239e2416eb
Glushko Oleksandr <stalevar.1990@gmail.com>
2025-04-17 21:44:50 +0300

    routing and parse body

--- basics/app.js
+++ basics/app.js
@@ -12,9 +12,29 @@ const server = http.createServer((request, response) => {
         response.setHeader('Content-Type', 'text/html');
         response.write('<html>');
         response.write('<head><title>My First Page</title></head>');
-        response.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+        response.write('<body><form action="/message" method="POST"><input name="message" type="text"></input><button type="submit">Send</button></form></body>');
         response.write('</html>');
         return response.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return request.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                if (err) {
+                    console.error(err);
+                    return response.end('Error writing file');
+                }
+            });
+            response.statusCode = 302;
+            response.setHeader('Location', '/');
+            return response.end();
+        });
     }
 
 
