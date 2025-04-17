a43d4f61af866ed00d916843d09494ab9bd62a8d
Glushko Oleksandr <stalevar.1990@gmail.com>
2025-04-18 00:43:33 +0300

    install express, getting middleware concept

--- express/app.js
+++ express/app.js
@@ -1,8 +1,23 @@
 
 const http = require('http');
+const express = require('express');
 
 
-const server = http.createServer();
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('Middleware 1');
+
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Middleware 2');
+
+  res.status(200).send('Hello from Express!');
+});
+
+const server = http.createServer(app);
 
 
 server.listen(3000, () => {
