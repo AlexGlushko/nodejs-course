9dc94e0a10ad98cb95ef6dca82cbfb3e6d8f7aca
Glushko Oleksandr <stalevar.1990@gmail.com>
2025-04-22 12:04:07 +0300

    serving html files

--- express/app.js
+++ express/app.js
@@ -7,7 +7,7 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).send('Home page');
+    res.status(200).sendFile(path.join(__dirname, '../', 'views', 'shop.html'), {
+        pageTitle: 'Shop',
+        hasProducts: false,
+        activeShop: true,
+        productCSS: true
+    });
   });
   
 
