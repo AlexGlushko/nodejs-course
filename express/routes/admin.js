dabaefd00d317c48f35adfd5ca2b6458ec7126ba
Glushko Oleksandr <stalevar.1990@gmail.com>
2025-04-22 16:49:16 +0300

    add path util for resovlve root dir, serving files

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
-
+const rootDir = require('../utils/path');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.status(200).sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.status(200).sendFile(path.join(rootDir, 'views', 'add-product.html'));
   });
 
 router.post('/product', (req, res, next) => {
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const rootDir = require('../utils/path');
 
 router.get('/', (req, res, next) => {
-    res.status(200).sendFile(path.join(__dirname, '../', 'views', 'shop.html'), {
+    res.status(200).sendFile(path.join(rootDir, 'views', 'shop.html'), {
         pageTitle: 'Shop',
         hasProducts: false,
         activeShop: true,
